ffc2d68041ebd4caa89ad79344e6926cc5cf8450
RaquelLima7 <raquellimadev@gmail.com>
2022-04-23 16:59:46 -0300

    added validates

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,11 @@
 class Customer < ApplicationRecord
+  has_paper_trail
+
+  validates :first_name, :last_name, :birthday, :country, presence: true
+  validate :birthday_date, on: %i[create update], if: :birthday
+
+  private
+  def birthday_date
+    errors.add(:birthday, ' must be an earlier date.') if birthday > Time.now
+  end
 end
